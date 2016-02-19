9e91f7c4a06e3ba64f336ff5daf89a6c502f8aeb
chitrakakkar <India@123>
2016-02-18 18:04:37 -0600

    started dinosaur program

---
+++ src/com/Chitra/Dainasour.java
@@ -0,0 +1,12 @@
+package com.Chitra;
+
+/**
+ * Created by chitrakakkar on 2/18/16.
+ */
+public class Dainasour
+{
+    public void roar()
+    {
+        System.out.println("Hello");
+    }
+}
---
+++ src/com/Chitra/Main.java
@@ -0,0 +1,14 @@
+package com.Chitra;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	 Dainasour t_rex = new Dainasour();
+        Dainasour velo = new Dainasour();
+
+        t_rex.roar();
+        velo.roar();
+    }
+}
